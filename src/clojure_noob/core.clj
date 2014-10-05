471265438736432f60e3ecaf8fd9d1066e9cefca
Travis <travismay@gmail.com>
2014-10-05 18:47:47 -0500

    Create body part symmetrization function

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -5,3 +5,66 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(defn no-mutation
+  [x]
+  (println x)
+
+  (let [x "Franz Kafka"]
+    (println x))
+
+  (println x))
+
+(defn sum
+  ([vals] (sum vals 0)) ;; ~~~1~~~
+  ([vals accumulating-total]
+     (if (empty? vals) ;; ~~~2~~~
+       accumulating-total
+       (sum (rest vals) (+ (first vals) accumulating-total)))))
+
+(defn inc-maker
+  "Create a custom incrementor"
+  [inc-by]
+  #(+ % inc-by))
+
+(def asym-hobbit-body-parts [{:name "head" :size 3}
+                             {:name "left-eye" :size 1}
+                             {:name "left-ear" :size 1}
+                             {:name "mouth" :size 1}
+                             {:name "nose" :size 1}
+                             {:name "neck" :size 2}
+                             {:name "left-shoulder" :size 3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest" :size 10}
+                             {:name "back" :size 10}
+                             {:name "left-forearm" :size 3}
+                             {:name "abdomen" :size 6}
+                             {:name "left-kidney" :size 1}
+                             {:name "left-hand" :size 2}
+                             {:name "left-knee" :size 2}
+                             {:name "left-thigh" :size 4}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "left-achilles" :size 1}
+                             {:name "left-foot" :size 2}])
+
+(defn has-matching-part?
+  [part]
+  (re-find #"^left-" (:name part)))
+
+(defn matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+(defn symmetrize-body-parts
+  "Expects a seq of maps which have a :name and :size"
+  [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts
+         final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts
+            final-body-parts (conj final-body-parts part)]
+        (if (has-matching-part? part)
+          (recur remaining (conj final-body-parts (matching-part part)))
+          (recur remaining final-body-parts))))))
